b8053c5dbef92b9084a44c7c1493065803cf1395
rvsharma-cmu <39546440+rvsharma-cmu@users.noreply.github.com>
2020-02-19 20:03:26 -0500

    testing adder.c

--- cgi-bin/adder.c
+++ cgi-bin/adder.c
@@ -20,16 +20,21 @@ void adder(int fd, char* args) {
     n2 = atoi(arg2);
 
     /* Make the response body */
+    sprintf(content, "Welcome to add.com: ");
+    sprintf(content, "%sTHE Internet addition portal.\r\n<p>", content);
     sprintf(content, "%sThe answer is: %d + %d = %d\r\n<p>",
             content, n1, n2, n1+n2);
     sprintf(content, "%sThanks for visiting!\r\n", content);
 
     /* Generate the HTTP response */
+    printf("Connection: close\r\n");
     printf("Content-length: %lu\r\n", strlen(content));
     printf("Content-type: text/html\r\n\r\n");
-    //printf("Content: %s\n", content);
-    printf("n1 + n2 = %d\n", n1+n2);
-    Rio_writen(fd, content, strlen(content));
+    printf("%s\n", content);
+//    printf("n1 + n2 = %d\n", n1+n2);
+    fflush(stdout);
+
+    rio_writen(fd, content, strlen(content));
     return;
 }
 
